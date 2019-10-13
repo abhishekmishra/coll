7e8d9cf17c467718d053c133fa5b202ff4c86fed
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-10-13 12:41:22 +0530

    added initial skeleton

---
+++ src/arraylist.h
@@ -0,0 +1,13 @@
+#ifndef ___ARRAYLIST_H___
+#define ___ARRAYLIST_H___
+
+#include <stdlib.h>
+
+typedef struct arraylist_t {
+	long capacity;
+} arraylist;
+
+int arraylist_new()
+
+
+#endif /* ___ARRAYLIST_H___ */
