617ae6aed0a583298925d01d45d98006187ca48e
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-10-15 12:31:52 +0530

    fixed test

--- test/test_arraylist.c
+++ test/test_arraylist.c
@@ -5,26 +5,27 @@
 #include <stdlib.h>
 
 #include "test_arraylist.h"
+#include "arraylist.h"
 
-static int group_setup(void **state)
-{
-    return 0;
+static int group_setup(void** state) {
+	return 0;
 }
-static int group_teardown(void **state)
-{
-    return 0;
-}
-
-static void test_bogus(void **state)
-{
-    assert_string_equal("bogus", "bogus");
+static int group_teardown(void** state) {
+	return 0;
 }
 
-int arraylist_tests()
-{
-    const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_bogus),
-    };
-    return cmocka_run_group_tests_name("docker container tests", tests,
-                                       group_setup, group_teardown);
+static void test_bogus(void** state) {
+	assert_string_equal("bogus", "bogus");
+}
+
+static void test_create_arraylist(void** state) {
+
+}
+
+int arraylist_tests() {
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(test_bogus),
+	};
+	return cmocka_run_group_tests_name("arraylist tests", tests,
+		group_setup, group_teardown);
 }
