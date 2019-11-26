b756502cfc619bce34dde88cfe16db599a315e46
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-11-26 19:02:29 +0530

    fixed vcpkg cmake path

--- build_linux.sh
+++ build_linux.sh
@@ -5,7 +5,7 @@ mkdir -p ./build
 cd ./build
 
 # generate the build files
-cmake .. -DCMAKE_TOOLCHAIN_FILE=${VCPKG_HOME}/vcpkg/scripts/buildsystems/vcpkg.cmake -DENABLE_TESTS=On -DENABLE_LUA=On
+cmake .. -DCMAKE_TOOLCHAIN_FILE=${VCPKG_HOME}/scripts/buildsystems/vcpkg.cmake -DENABLE_TESTS=On -DENABLE_LUA=On
 
 # build
 make clean all
